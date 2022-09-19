f748bba4b43a77acf8a00a3467303784665cad0f
Kshibata14 <113054100+Kshibata14@users.noreply.github.com>
2022-09-19 14:38:00 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,51 @@
 input.onButtonPressed(Button.A, function () {
-    Number2 = randint(0, 2)
-    if (Number2 == 0) {
-        basic.showIcon(IconNames.Angry)
-    } else if (Number2 == 1) {
-        basic.clearScreen()
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(262, music.beat(BeatFraction.Whole))
-    } else {
-        basic.showIcon(IconNames.Ghost)
+    radio.sendString(Yellow)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . # . .
+        # . . . #
+        . # . # .
+        `)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == Yellow) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # . # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    if (receivedString == AnimateYellow) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                # . # . #
+                . # # # .
+                # # . # #
+                . # # # .
+                # . # . #
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . # . # .
+                # . . . #
+                . . # . .
+                # . . . #
+                . # . # .
+                `)
+            basic.pause(100)
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    Number2 = randint(3, 5)
-    if (Number2 == 3) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showIcon(IconNames.SmallSquare)
-        basic.showIcon(IconNames.Square)
-    } else if (Number2 == 4) {
-        basic.clearScreen()
-        music.playMelody("E D F G E B A F ", 120)
-    } else {
-        basic.showIcon(IconNames.Asleep)
-    }
+    radio.sendString(AnimateYellow)
 })
-let Number2 = 0
-basic.showIcon(IconNames.Heart)
-basic.forever(function () {
-	
-})
+let AnimateYellow = ""
+let Yellow = ""
+Yellow = "Yellow"
+AnimateYellow = "AnimiateYellow"
+radio.setGroup(23)
